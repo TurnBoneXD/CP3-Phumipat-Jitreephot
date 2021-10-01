e96425d04335abac569628f3a319f0e87689b4bf
Turbo <Phumipat33628@gmail.com>
2021-10-01 19:44:55 +0700

    EX4

--- Exercise4_Phumipat_J.py
+++ Exercise4_Phumipat_J.py
@@ -1,1 +1,14 @@
-print("hi")
+scoreFE = float(18)
+scoreGB = float(26)
+scoreICS = float(34)
+scoreCP = float(9)
+#scoreFE = Foundation English
+#scoreGB = General Business
+#scoreICS = Introduction to Computer Systems
+#scoreCP = Computer Programming
+
+print("--- Your Score ---")
+print("Foundation English :", scoreFE, "คะแนน")
+print("General Business :", scoreGB, "คะแนน")
+print("Introduction to Computer Systems :", scoreICS, "คะแนน")
+print("Computer Programming :", scoreCP, "คะแนน")
