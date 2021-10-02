13ff7cf0e9ec1d99dde3cb3daa09d53e2482ea45
Turbo <Phumipat33628@gmail.com>
2021-10-02 21:38:35 +0700

    Create Exercise9_Phumipat_J.py

---
+++ Exercise9_Phumipat_J.py
@@ -0,0 +1,7 @@
+usernameInput = input("Username : ")
+passwordInput = input("Password : ")
+while usernameInput  != "Turbo"  or passwordInput != "33628":
+    print("Wrong username or password")
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    print("Done")
